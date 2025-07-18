bd48e9e3956c4f168c10cb88bfd33cd572212966
Taichi Ishikawa <ishikawataichi1@gmail.com>
2025-07-18 19:46:10 +0900

    feat: create sample flask

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return jsonify(message="flask root")
+
+
+@app.route("/api/hello")
+def hello():
+    return jsonify(message="Hello from Flask on Fargate!")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
